7b442b1beff5c61ce56dd5e6ae1ddf4bbfc4b321
jzChia <zhuxi145@163.com>
2017-07-22 15:39:33 +0800

    test

--- src/main/java/com/example/demo/OauthtestApplication.java
+++ src/main/java/com/example/demo/OauthtestApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OauthtestApplication {
 
+
 	public static void main(String[] args) {
+
+		System.out.println("");
 		SpringApplication.run(OauthtestApplication.class, args);
 	}
 }
